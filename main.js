9509865152653551bbeb52b4d88f0fd421e90fa9
NyznTv <146996463+NyznTv@users.noreply.github.com>
2023-10-10 15:34:34 +0200

    Update and rename voices.js to main.js

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 {
   "searchStrings": [["hugo","letshugo","letshugotv","ügo"],["basti","bastighg","bastian"]],
   "searchIcons": [" :",":"," =","="," >",">"],
-  "voiceIds": {"bot":"21m00Tcm4TlvDq8ikWAM","hugo":"CYw3kZ02Hs0563khs1Fj","basti":"D38z5RcWu1voky8WS1ja"},
-  "test":"Ja"
+  "voiceIds": {"bot":"21m00Tcm4TlvDq8ikWAM","hugo":"CYw3kZ02Hs0563khs1Fj","basti":"D38z5RcWu1voky8WS1ja"}
 }
