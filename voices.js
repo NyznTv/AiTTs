d3f99f89401324308051e85973f9c0c0e8fe6252
NyznTv <146996463+NyznTv@users.noreply.github.com>
2023-10-10 15:33:06 +0200

    Update voices.js

--- voices.js
+++ voices.js
@@ -1,5 +1,6 @@
 {
   "searchStrings": [["hugo","letshugo","letshugotv","ügo"],["basti","bastighg","bastian"]],
   "searchIcons": [" :",":"," =","="," >",">"],
-  "voiceIds": {"bot":"21m00Tcm4TlvDq8ikWAM","hugo":"CYw3kZ02Hs0563khs1Fj","basti":"D38z5RcWu1voky8WS1ja"}
+  "voiceIds": {"bot":"21m00Tcm4TlvDq8ikWAM","hugo":"CYw3kZ02Hs0563khs1Fj","basti":"D38z5RcWu1voky8WS1ja"},
+  "test":"Ja"
 }
