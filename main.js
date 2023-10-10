7bd40033f73b6a6f4b07cf442d5d6b500c31327f
NyznTv <146996463+NyznTv@users.noreply.github.com>
2023-10-10 21:32:57 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 {
-    "searchStrings": [["dilara","di1araas","dilaraas","dilaras","dilosch"],["krieger","cmdkrieger","commanderkrieger","commander"]],
+    "searchStrings": [["dilara","di1araas","dilaraas","dilaras","dilosch"],["krieger","cmdkrieger","commanderkrieger","commander"],["cro","carlo","carl"],["paluten","palle","patrick","patrik"]],
     "searchIcons": [" :",":"," =","="," >",">"],
-    "voiceIds": {"bot":"DHO3QwxR0XwQf2oA5V9m","dilara":"RqPGnLrbxmUSVtCqjMbf","krieger":"K0X7026Q2QwpTPkBsw5X"}
+    "voiceIds": {"bot":"DHO3QwxR0XwQf2oA5V9m","dilara":"RqPGnLrbxmUSVtCqjMbf","krieger":"K0X7026Q2QwpTPkBsw5X","cro":"2DPhbJ2hDKB8zb0FjPMm","paluten":"6ltPA5r9EEDkVsJZAmCU"}
 }
